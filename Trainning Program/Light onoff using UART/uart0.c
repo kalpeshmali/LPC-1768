fa2ffeacb51685f7f6130df504f34ccfd1a225bc
kalpeshmali <45162354+kalpeshmali@users.noreply.github.com>
2019-06-06 12:46:24 +0530

    Using uart LED is On and Off

---
+++ Trainning Program/Light onoff using UART/uart0.c
@@ -0,0 +1,27 @@
+#include "LPC17xx.h"
+void initialization(void);
+int main()
+{                                                                         
+					SystemInit();
+					uint32_t val;
+				initialization();
+					while(1)
+					{
+							while(((LPC_UART0->LSR)&0x01)==0x01) 
+							{
+									val= LPC_UART0->RBR;
+
+									if(((LPC_UART0->LSR)&0x20)==0x20)   
+											LPC_UART0->THR=val; 
+									if(val == 'a')
+									{
+											LPC_GPIO1->FIOSET3 =LPC_GPIO1->FIOSET3  | (1<<5);
+									}
+									else if(val == 'b')
+									{
+											LPC_GPIO1->FIOCLR3 =LPC_GPIO1->FIOCLR3  | (1<<5);
+									}
+							}
+					}
+}
+	
